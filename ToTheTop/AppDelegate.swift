1b08000c5a6950f2e424fb9804f90567361169bf
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-03-08 17:48:29 +0100

    Add empty Xcode project

---
+++ ToTheTop/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ToTheTop
+//
+//  Created by Christoffer Winterkvist on 3/8/17.
+//  Copyright © 2017 Christoffer Winterkvist. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
